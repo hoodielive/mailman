3b15249229e8db0c3163f6eac27bfcc85ef7a9e9
hoodielive <hoodielive@gmail.com>
2018-09-08 22:00:49 -0400

    correction - but now the final

--- refactor.sh
+++ refactor.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash 
 
-set -x 
-
 OLD_EMAIL=$1
 NEW_EMAIL=$2 
 VALID_CHARS="([a-zA-Z])+@(email\.lab|penguin\.lab)$"
@@ -25,11 +23,11 @@ if [[ $OLD_EMAIL =~ $VALID_CHARS ]] && [[ $NEW_EMAIL =~ $VALID_CHARS ]]; then
   sudo -u mailman /usr/lib/mailman/bin/find_member "$OLD_EMAIL" | while read lists; do
     lists=$lists
     [[ $lists == *found* ]] && continue 
-    echo $NEW_EMAIL | sudo -u mailman /usr/lib/mailman/bin/add_members --regular-members-file=- --welcome-sgn=n --admin-notify=n "$lists"
+    echo $NEW_EMAIL | sudo -u mailman /usr/lib/mailman/bin/add_members --regular-members-file=- --admin-notify=n "$lists"
     echo "$NEW_EMAIL now subscribed to $lists"
   done
   # now purge this email address
-  echo $OLD_EMAIL | sudo -u mailman /usr/lib/mailman/bin/remove_members --file=- --fromall -nouserack --noadminack
+  echo $OLD_EMAIL | sudo -u mailman /usr/lib/mailman/bin/remove_members --file=- --fromall --nouserack --noadminack
   echo "$OLD_EMAIL is now deleted"
 else 
   usage
