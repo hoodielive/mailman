0076afa2361fe2d9eab16b0b878de3dad58de9d3
hoodielive <hoodielive@gmail.com>
2018-09-23 10:16:42 -0400

    poc-script

--- poc_script.sh
+++ poc_script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash 
 
 usage() {
-  script.sh [options] arg1 arg2 
-
+  echo "script.sh [options] arg1 arg2"
 }
 
 while getopts "h:u:p:d:" flag; do
@@ -17,12 +16,43 @@ done
 ARG1=${@:$OPTIND:1}
 ARG2=${@:$OPTIND+1:1}
 
+if [[ "$#" -ne 2 ]]
+then
+  usage
+  exit 220 
+fi
+
 if [[ "$HOSTNAME" ]]
 then
   echo "Hostname is $(hostnamectl)"
 else
-  echo "something went wrong"
+  usage 
+  exit 221 
+fi
+
+if [[ "$USERNAME" ]]
+then
+  echo "You are user $(echo $USER)"
+else 
+  usage 
+  exit 222 
+fi
+
+if [[ "$PASSWORD" ]]
+then
+  echo "Your password is: doodle"
+else
+  usage 
+  exit 223 
+fi
+
+if [[ "$DATABASE" ]]
+then
+  echo "This is a database called: mongodb"
+else
+  echo "Something went wrong"
   usage
+  exit 224
 fi
 
 
