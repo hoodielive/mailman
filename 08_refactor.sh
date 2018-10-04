9e8e5af51683091453d0ed0e27090a0c0115b6aa
hoodielive <hoodielive@gmail.com>
2018-10-04 18:11:33 -0400

    working through /bin/sh madness

---
+++ 08_refactor.sh
@@ -0,0 +1,146 @@
+#!/usr/bin/env sh
+
+# Unset Vars 
+unset APPNAME; 
+unset DEBUG; 
+unset VERSION; 
+unset DRY;
+unset OLD_EMAIL; 
+unset NEW_EMAIL;
+unset VALID_CHARS; 
+unset FIND_MEMBER; 
+unset ADD_MEMBER; 
+unset REMOVE_MEMBER; 
+
+# Set Vars 
+APPNAME="userid-refactor"
+DEBUG=false; 
+VERSION="0.1.0"
+DRY=false;
+OLD_EMAIL=$2
+NEW_EMAIL=$3
+VALID_CHARS="^([a-zA-Z0-9])+@(email\.lab|penguin\.lab)$"
+FIND_MEMBER="/usr/lib/mailman/bin/find_member"
+ADD_MEMBER="/usr/lib/mailman/bin/add_members"
+REMOVE_MEMBER="/usr/lib/mailman/bin/remove_members"
+
+# Functions 
+usage() {
+cat << EOF
+Usage: userid-refactor [-h help] [-d DEBUG] [-v verbose] [-f refactor]
+
+DESCRIPTION
+
+-h returns usage information
+
+-d turns on DEBUG mode
+
+-v turns on verbosity
+
+-V version number
+
+-n dry-run
+
+-f migrate old-email-address lists to new-email-adddress lEists
+
+EOF
+}
+
+debug() 
+{
+  [ "${DEBUG}" = 'true' ] && echo "${@}" >&2 
+  return 0 
+}
+
+verbose() 
+{
+  if [ "$VERBOSE" = 'true' ]; then
+    sleep 1
+    echo "$@." || VERBOSE=false; echo
+  fi
+  return 0
+}
+
+while getopts "hdvVnf:" opt; do
+  case ${opt} in
+     h) HELP='true'; usage ;; 
+     d) DEBUG='true' ;;
+     v) VERBOSE='true' ;; 
+     V) verbose "Requested version of ${APPNAME}"; echo "${APPNAME}" "${VERSION}";  exit;; 
+     n) DRY='true'; set -vn ;; 
+     f) REFACTOR=$OPTARG ;;
+     *) usage; exit ;; 
+  esac 
+done 
+
+shift $((OPTIND -1))
+
+verbose "Option Parsing" 
+# parse email address args, query old
+# subscriptions, bind results from queries to the new email address.
+if [ ! -z "$REFACTOR" ]; then
+  verbose "checking $OLD_EMAIL and $NEW_EMAIL"
+
+  if  echo "$OLD_EMAIL" | grep -E -q "$VALID_CHARS"; then  
+    verbose "$OLD_EMAIL meets the valid email requirement"
+  else
+    verbose "$OLD_EMAIL does NOT meet the requirement for email addresses"
+    debug "$OLD_EMAIL does NOT meet validation, please check requirements"
+    exit 13
+  fi
+
+  if echo "$NEW_EMAIL" | grep -E -q "$VALID_CHARS"; then 
+    verbose "$NEW_EMAIL meets the valid email requirement"
+  else
+    verbose "$NEW_EMAIL does NOT meet the requirement for email addresses"
+    debug "$NEW_EMAIL does NOT meet validation, please check requirements"
+    exit 13
+  fi
+
+
+# Query OLD email subscriptions. 
+
+if sudo -u list "${FIND_MEMBER}" "$OLD_EMAIL"; then
+   verbose "Found ${OLD_EMAIL} subscriptions" 
+else 
+   DEBUG=true 
+   debug "$OLD_EMAIL does NOT seem to be subscribed to any lists"
+   exit 13
+fi
+
+sudo -u list "${FIND_MEMBER}" "$OLD_EMAIL" | while read -r lists; do
+if "$(grep -E "found")"; then
+    continue;
+  else
+    DEBUG=true 
+    debug "something went wrong in the while loop!"
+    exit 13
+ fi
+
+  verbose "Migrating ${OLD_EMAIL} lists to ${NEW_EMAIL}"
+
+  echo "$NEW_EMAIL" | sudo -u list "${ADD_MEMBER}" --regular-members-file=- --admin-notify=n "$lists"
+
+  verbose "${NEW_EMAIL} now subscribed to $lists"
+
+done
+ verbose "Migration complete"
+
+else 
+  DEBUG=true
+  debug "Could not migrate member."
+  exit 13
+fi 
+
+# Delete old email address. 
+verbose "Now that $OLD_EMAIL lists have been migrated, lets purge $OLD_EMAIL" 
+
+echo "$OLD_EMAIL" | sudo -u list "${REMOVE_MEMBER}" --file=- --fromall --nouserack --noadminack
+
+# Confirm that old email address has been deleted, let us know if it's not. 
+if [ "${REMOVE_MEMBER} ${OLD_EMAIL}" -a "$?" -eq 1 ]; then
+   verbose "$OLD_EMAIL is now deleted"
+ else
+   DEBUG=true
+   debug "$OLD_EMAIL is not deleted"
+fi
--- lets.sh
+++ lets.sh
@@ -10,7 +10,7 @@ valid_chars="^([a-zA-Z0-9])+@(email\.lab)$"
 REFACTOR=""
 DEBUG=false
 VERBOSE=false
-MESSAGE=${@}
+#MESSAGE=${@}
 
 debug()
 {
@@ -18,15 +18,30 @@ debug()
   return 0 
 }
 
-verbose() 
-{
-  #local MESSAGE="${@}"
-  [ "x${VERBOSE}" = x ] && return; 
-  while [ $# -gt 0 ]; do 
-    echo "script : info: ${1}" 1>&2
-    shift
-  done
+#verbose() 
+#{
+#  #local MESSAGE="${@}"
+#  [ "x${VERBOSE}" = x ] && return; 
+#  while [ $# -gt 0 ]; do 
+#    echo "script : info: ${1}" 1>&2
+#    shift
+#  done
+#}
+
+verbose() { 
+  [ "$VERBOSE" = 'true' ] && log_action_msg "$@"; 
+  return 0 
 }
+
+log_action_msg () {
+    log_action_msg_pre "$@"
+    echo "$@." || true
+    log_action_msg_post "$@"
+}
+
+log_action_msg_pre () { :; }
+
+log_action_msg_post () { :; }
 
 while getopts "hdvf:" opt; do 
   case $opt in 
@@ -52,12 +67,13 @@ shift $((OPTIND - 1))
 
 if [ ! -z "$REFACTOR" ]; then 
   #if [[ "$old_email" =~ $valid_chars  ]]; then #| grep -Eq "$valid_chars"; then 
-  if expr "x$old_email" : "x$valid_chars" > /dev/null; then
+  #if expr "x$old_email" : "x$valid_chars" > /dev/null; then
+  #  verbose "confirming that the old email address meets valid character check"
+  #else
+  #  debug "test failed"
+  #fi
+  echo $old_email | egrep -q "^([a-zA-Z0-9])+@(email\.lab)$"
     verbose "confirming that the old email address meets valid character check"
-  else
-    debug "test failed"
-  fi
-
   #if [[ "$new_email" =~ $valid_chars ]]; then 
   if expr "x$new_email" : "x$valid_chars" > /dev/null; then 
     verbose "confirming that the new email address meets valid character check"
